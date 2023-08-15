ce88233dfc085b7468be8d4f9fb517aa1cb319eb
Chehak <chehakbatra5911@gmail.com>
2023-08-15 18:39:14 +0530

    create login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
+
+    <Router>
+      <Routes>
+        <Route exact path='/' element={ <Login /> } />
+      </Routes>
+    </Router>
       
     </div>
   );
