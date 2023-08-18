474e27f2e4558f9926d870595618e26d2dba61f7
Chehak <chehakbatra5911@gmail.com>
2023-08-18 17:46:53 +0530

    Create the Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';;
 
 function App() {
   return (
     <div className="App">
 
-    <Router>
-      <Routes>
-        <Route exact path='/' element={ <Login /> } />
-      </Routes>
-    </Router>
-      
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Login />} />
+          <Route path='/home' element={<div> <Header /> <Home /> </div>} />
+
+        </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import React from 'react'
+import Header from './Header';
+
+function Home(props) {
+  return (
+    <div>Home</div>
+    
+  )
+}
+
+export default Home;
